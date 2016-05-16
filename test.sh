6ec4b9602e63555a3612218bc0daa7a7f0a4104b
Kuba Tyszko <kuba@lbl.pl>
2016-05-17 07:55:25 +0900

    added serial reset - requires pyserial library for python

--- test.sh
+++ test.sh
@@ -4,4 +4,6 @@ export avrdudeconf=/Applications/Arduino.app/Contents/Java/hardware/tools/avr/et
 export uart=/dev/tty.usbmodem1421
 export hexfile=test_arduboy_10.hex
 
+python -c "import serial;ser=serial.Serial('/dev/tty.usbmodem1421',1200);ser.write('');ser.close()"
+sleep 2
 avrdude -C${avrdudeconf} -v -patmega32u4 -cavr109 -P${uart} -b57600 -D -Uflash:w:${hexfile}:i
