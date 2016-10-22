ef2df09d94c50091c3e3c5c13ce189ef29df6971
Kuba Tyszko <kuba@lbl.pl>
2016-10-22 07:33:40 -0700

    making the script work on linux

--- test.sh
+++ test.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-export avrdudeconf=/Applications/Arduino.app/Contents/Java/hardware/tools/avr/etc/avrdude.conf
-export uart=/dev/tty.usbmodem1421
+if [ `uname -s` = "Darwin" ]; then
+	export avrdudeconf=/Applications/Arduino.app/Contents/Java/hardware/tools/avr/etc/avrdude.conf
+	export uart=/dev/tty.usbmodem1421
+	python -c "import serial;ser=serial.Serial('/dev/tty.usbmodem1421',1200);ser.write('');ser.close()"
+else
+	export avrdudeconf=/opt/arduino/tools/avr/etc/avrdude.conf
+	export uart=/dev/tty.usbserial
+	python -c "import serial;ser=serial.Serial('/dev/tty.usbserial',1200);ser.write('');ser.close()"
+fi
+
 export hexfile=test.hex
 
-python -c "import serial;ser=serial.Serial('/dev/tty.usbmodem1421',1200);ser.write('');ser.close()"
 sleep 2
 avrdude -C${avrdudeconf} -v -patmega32u4 -cavr109 -P${uart} -b57600 -D -Uflash:w:${hexfile}:i
+
